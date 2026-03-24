5f3b4245b50b5892cfe39a788fcaedb6dc96ead5
Acid <sudiptochakrabortysuvo@gmail.com>
2026-03-24 21:38:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,34 @@ const app = express();
 
 const PORT = process.env.PORT || 10000;
 
-app.use(express.static(path.join(__dirname)));
-
-app.get('/', (req, res) => {
+// 1. Check for curl BEFORE serving static files
+app.get('/', (req, res, next) => {
     const userAgent = req.headers['user-agent'] || '';
-
+    
     if (userAgent.toLowerCase().includes('curl')) {
-        // This is the Plain Text version for your Linux Terminal
+        res.setHeader('Content-Type', 'text/plain');
         return res.send(`
 \x1b[31macid\x1b[0m@\x1b[31mGenesis\x1b[0m
 -----------------------
 \x1b[31mUser\x1b[0m   > Sudipto Chakraborty
-\x1b[31mOS\x1b[0m     > Ubuntu 24.04 LTS
+\x1b[31mOS\x1b[0m     > Ubuntu 24.04 LTS (Genesis)
+\x1b[31mKernel\x1b[0m > Linux 6.17.0-generic
+\x1b[31mShell\x1b[0m  > zsh / bash
 \x1b[31mGithub\x1b[0m > github.com/acidki
-\x1b[31mStatus\x1b[0m > Online (DIU)
+\x1b[31mStatus\x1b[0m > Online (Daffodil Intl University)
 
-Try: curl ${req.headers.host}/help
+\x1b[32m[ Website: https://${req.headers.host} ]\x1b[0m
 `);
     }
-    
+    // If not curl, move to the next part (serving the website)
+    next();
+});
+
+// 2. Serve the browser files
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server live on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
